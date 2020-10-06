1a8b6225cd8af798893541e33fe6da61cc895cfd
Dmytro Maletskyi <dmytro.maletskiy@gmail.com>
2020-10-06 20:20:10 +0300

    Разбивает код на модули

---
+++ js/gallery.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(() => {
+  const picturesListElement = document.querySelector(`.pictures`);
+
+  const renderPicturesList = (picturesList) => {
+    const fragment = document.createDocumentFragment();
+
+    for (let picture of picturesList) {
+      fragment.appendChild(window.picture.renderPictureElement(picture));
+    }
+
+    picturesListElement.appendChild(fragment);
+  };
+
+  window.gallery = {
+    renderPicturesList
+  }
+})();
---
+++ js/utils.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(() => {
+  const getRandomIndex = (arrayLength) => Math.floor(Math.random() * arrayLength);
+
+  const getRandomInt = (min = 15, max = 200) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+  window.utils = {
+    getRandomIndex,
+    getRandomInt
+  }
+})();
